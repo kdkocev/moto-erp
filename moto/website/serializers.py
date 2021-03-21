b7624d3c92fc74db31e714f46215bafc11efe66c
kdkocev <kkotsev94@gmail.com>
2021-03-21 22:37:47 +0200

    Clean up the serializers

--- moto/website/serializers.py
+++ moto/website/serializers.py
@@ -9,22 +9,6 @@ class OrderSerializer(serializers.ModelSerializer):
         fields = '__all__'
         extra_kwargs = {'id': {'read_only': True}}
 
-    def create(self, validated_data):
-        return Order.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.order_number = validated_data.get('order_number', instance.order_number)
-        instance.part = validated_data.get('part', instance.part)
-        instance.amount = validated_data.get('amount', instance.amount)
-        instance.date_received = validated_data.get('date_received', instance.date_received)
-        instance.date_of_expedition = validated_data.get('date_of_expedition', instance.date_of_expedition)
-        instance.date_of_delivery = validated_data.get('date_of_delivery', instance.date_of_delivery)
-        instance.completed_at = validated_data.get('completed_at', instance.completed_at)
-        instance.created_at = validated_data.get('created_at', instance.created_at)
-        instance.save()
-
-        return instance
-
 
 class PartSerializer(serializers.ModelSerializer):
     class Meta:
@@ -32,33 +16,12 @@ class PartSerializer(serializers.ModelSerializer):
         fields = '__all__'
         extra_kwargs = {'id': {'read_only': True}}
 
-    def create(self, validated_data):
-        return Part.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.number = validated_data.get('number', instance.number)
-        instance.price_total = validated_data.get('price_total', instance.price_total)
-        instance.price_machining = validated_data.get('price_machining', instance.price_machining)
-        instance.casting = validated_data.get('casting', instance.casting)
-        instance.save()
-
-        return instance
-
 
 class CastingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Casting
         fields = '__all__'
         extra_kwargs = {'id': {'read_only': True}}
-
-    def create(self, validated_data):
-        return Casting.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.number = validated_data.get('number', instance.number)
-        instance.save()
-
-        return instance
 
 
 class ExpeditionSerializer(serializers.ModelSerializer):
